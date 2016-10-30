d959e74da4613b0a661bcd7f1bf07e22ad119760
Kaushik Veluru <kaushikveluru@gmail.com>
2016-10-30 14:19:39 -0400

    4 dummy controllers implemented

---
+++ public/assignment/views/user/user.controller.client.js
@@ -0,0 +1,19 @@
+(function(){
+    angular
+        .module("WebAppMaker")
+        .controller("LoginController",LoginController)
+        .controller("RegisterController",RegisterController)
+        .controller("ProfileController",ProfileController)
+
+    function LoginController(){
+
+    }
+
+    function RegisterController(){
+
+    }
+
+    function ProfileController(){
+
+    }
+})();
