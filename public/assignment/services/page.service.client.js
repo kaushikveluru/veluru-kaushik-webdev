f9c0071b157177e2f5e90745c4ff5bf8b111c84d
Kaushik Veluru <kaushikveluru@gmail.com>
2016-11-02 01:55:47 -0400

    implemented all functions in PageService

--- public/assignment/services/page.service.client.js
+++ public/assignment/services/page.service.client.js
@@ -22,22 +22,55 @@
         return api;
 
         function createPage(websiteId, page){
-
+            page.websiteId = websiteId;
+            pages.push(page);
         }
 
         function findPageByWebsiteId(websiteId){
+            for(var p in pages)
+            {
+                if(pages[p].websiteId === websiteId)
+                {
+                    return pages[p]
+                }
 
+            }
+            return null;
         }
 
         function findPageById(pageId){
+            for(var p in pages)
+            {
+                if(pages[p]._Id === pageId)
+                {
+                    return pages[p]
+                }
 
+            }
+            return null;
         }
 
         function updatePage(pageId,page){
+            for(var p in pages)
+            {
+                if(pages[p]._Id === pageId)
+                {
+                     pages[p] = page;
+                }
+
+            }
 
         }
 
         function deletePage(pageId){
+            for(var p in pages)
+            {
+                if(pages[p]._Id === pageId)
+                {
+                    pages[p].remove();
+                }
+
+            }
 
         }
     }
