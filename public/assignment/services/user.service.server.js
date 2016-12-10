9fe88ebec32b463c0138aca2320215fafc9542ff
Kaushik Veluru <kaushikveluru@gmail.com>
2016-12-10 17:36:25 -0500

    added skeleton website webservice

--- public/assignment/services/user.service.server.js
+++ public/assignment/services/user.service.server.js
@@ -38,6 +38,6 @@ module.exports = function(app){
     }
 
     function deleteUser(req,res){
-        
+
     }
 }
--- public/assignment/services/website.service.server.js
+++ public/assignment/services/website.service.server.js
@@ -1,1 +1,33 @@
 
+module.exports=function(app){
+    var websites=[
+        { "_id": "123", "name": "Facebook",    "developerId": "456", "description": "Lorem" },
+        { "_id": "234", "name": "Tweeter",     "developerId": "456", "description": "Lorem" },
+        { "_id": "456", "name": "Gizmodo",     "developerId": "456", "description": "Lorem" },
+        { "_id": "567", "name": "Tic Tac Toe", "developerId": "123", "description": "Lorem" },
+        { "_id": "678", "name": "Checkers",    "developerId": "123", "description": "Lorem" },
+        { "_id": "789", "name": "Chess",       "developerId": "234", "description": "Lorem" }
+    ]
+
+    app.post("/api/user/:userId/website", createWebsite);
+    app.get("/api/user/:userId/website", findAllWebsitesForUser);
+    app.get("/api/website/:websiteId", findWebsiteById);
+    app.put("/api/website/:websiteId", updateWebsite);
+    app.delete("/api/website/:websiteId", deleteWebsite);
+
+    function createWebsite(req,res){
+
+    }
+    function findAllWebsitesForUser(req,res){
+
+    }
+    function findWebsiteById(req,res){
+
+    }
+    function updateWebsite(req,res){
+
+    }
+    function deleteWebsite(req,res){
+
+    }
+}
