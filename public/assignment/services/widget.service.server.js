ee2409f78b394daab179028e46ab6776a09e0ba6
Kaushik Veluru <kaushikveluru@gmail.com>
2016-12-10 17:43:31 -0500

    added skeleton for widget webservice

--- public/assignment/services/widget.service.server.js
+++ public/assignment/services/widget.service.server.js
@@ -1,3 +1,42 @@
-/**
- * Created by kaush_000 on 12/10/2016.
- */
+module.exports = function(app){
+    var widgets=[
+        { "_id": "123", "widgetType": "HEADER", "pageId": "543", "size": 2, "text": "GIZMODO"},
+        { "_id": "234", "widgetType": "HEADER", "pageId": "543", "size": 4, "text": "Lorem ipsum"},
+        { "_id": "345", "widgetType": "IMAGE", "pageId": "543", "width": "100%",
+            "url": "http://lorempixel.com/400/200/"},
+        { "_id": "456", "widgetType": "HTML", "pageId": "543", "text": "<p>Lorem ipsum</p>"},
+        { "_id": "567", "widgetType": "HEADER", "pageId": "543", "size": 4, "text": "Lorem ipsum"},
+        { "_id": "678", "widgetType": "YOUTUBE", "pageId": "543", "width": "100%",
+            "url": "https://youtu.be/AM2Ivdi9c4E" },
+        { "_id": "789", "widgetType": "HTML", "pageId": "543", "text": "<p>Lorem ipsum</p>"}
+    ]
+
+
+
+    app.post("/api/widget/:pageId/widget", createWidget);
+    app.get("/api/widget/:pageId/widget", findAllWidgetsForPage);
+    app.get("/api/widget/:widgetId", findWidgetById);
+    app.put("/api/widget/:widgetId", updateWidget);
+    app.delete("/api/widget/:widgetId", deleteWidget);
+
+    function createWidget(req,res){
+
+    }
+
+    function findAllWidgetsForPage(req,res){
+
+    }
+
+    function findWidgetById(req,res){
+
+    }
+
+    function updateWidget(req,res){
+
+    }
+
+    function deleteWidget(req,res){
+
+    }
+
+}
