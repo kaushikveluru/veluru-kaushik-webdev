98f4565f6a607b13daebe7d3aa31191aff01eee3
Kaushik Veluru <kaushikveluru@gmail.com>
2016-12-12 15:44:56 -0500

    fixed mongoose connection error

--- assignment/models/user/user.model.server.js
+++ assignment/models/user/user.model.server.js
@@ -1,7 +1,7 @@
 module.exports = function () {
     var model = {};
     var mongoose = require("mongoose");
-    var UserSchema = require("./user.schema.server")();
+    var UserSchema = require("./user.schema.server.js")();
     var UserModel  = mongoose.model("UserModel", UserSchema);
 
     var api = {
